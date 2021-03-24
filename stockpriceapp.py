28d84327fba4255ddd694cb0400a244a507313cc
amirzhangirbayev <81210371+amirzhangirbayev@users.noreply.github.com>
2021-03-24 14:45:37 +0500

    Add files via upload

---
+++ stockpriceapp.py
@@ -0,0 +1,50 @@
+import os
+import streamlit as st
+import yfinance as yf
+import pandas as pd
+os.system('cls')
+
+######################
+# Page Title
+######################
+
+st.write('''
+# Simple Stock Price App
+
+The stock closing price and volume of Google
+
+***
+''')
+
+######################
+# Main Page
+######################
+
+google = yf.Ticker('GOOGL')
+tickerDf = google.history(period='1d', start='2015-5-31', end='2017-5-31')
+
+# Business Summary
+st.write('''
+## Business Summary
+''')
+
+st.write(google.info['longBusinessSummary'])
+
+# Closing Price
+st.write('''
+## Closing Price
+''')
+
+st.line_chart(tickerDf.Close)
+
+# Volume
+st.write('''
+## Volume
+''')
+
+st.line_chart(tickerDf.Volume)
+
+
+
+
+
